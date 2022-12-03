e3b0a25bcd3292d053308e28e1049d6e2c24a2d8
itliujia <2264284303@qq.com>
2022-12-03 11:43:45 +0800

    init

---
+++ src/layout/components/footer.vue
@@ -0,0 +1,33 @@
+<template>
+    <footer class="layout-footer">
+        <div class="text-center p-2 text-xs text-tx-secondary max-w-[900px] mx-auto">
+            <el-link @click="url(item.value)" :underline="false" v-for="item in copyright" :key="item.key">
+                {{ item.key }}</el-link>
+        </div>
+    </footer>
+</template>
+
+<script setup lang="ts">
+import useAppStore from '@/stores/modules/app'
+import { filterAsyncRoutes } from '@/router'
+
+const appStore = useAppStore()
+const copyright = computed(() => appStore.config.copyright_config || [])
+
+function url(params: string) {
+    if (params) {
+        window.location.href = params
+    }
+}
+</script>
+
+
+<style>
+.p-2 {
+    margin-bottom: 50px;
+}
+
+a:hover {
+    text-decoration: none
+}
+</style>
---
+++ src/views/setting/website/information.vue
@@ -0,0 +1,106 @@
+<!-- 网站信息 -->
+<template>
+    <div class="website-information">
+        <el-form ref="formRef" :rules="rules" class="ls-form" :model="formData" label-width="120px">
+            <el-card shadow="never" class="!border-none">
+                <el-form-item label="网站名称" prop="name">
+                    <div class="w-80">
+                        <el-input v-model.trim="formData.name" placeholder="请输入网站名称" maxlength="30" show-word-limit />
+                    </div>
+                </el-form-item>
+                <el-form-item label="网站图标" prop="web_favicon" required>
+                    <div>
+                        <material-picker v-model="formData.web_favicon" :limit="1" />
+                        <div class="form-tips">建议尺寸：100*100像素，支持jpg，jpeg，png格式</div>
+                    </div>
+                </el-form-item>
+                <el-form-item label="网站LOGO" prop="web_logo" required>
+                    <div>
+                        <material-picker v-model.trim="formData.web_logo" :limit="1" />
+                        <div class="form-tips">建议尺寸：100*100像素，支持jpg，jpeg，png格式</div>
+                    </div>
+                </el-form-item>
+                <el-form-item label="登录页广告图" prop="login_image" required>
+                    <div>
+                        <material-picker v-model.trim="formData.login_image" :limit="1" />
+                        <div class="form-tips">建议尺寸：100*100像素，支持jpg，jpeg，png格式</div>
+                    </div>
+                </el-form-item>
+            </el-card>
+        </el-form>
+        <footer-btns v-perms="['setting.web.web_setting/setWebsite']">
+            <el-button type="primary" @click="handleSubmit">保存</el-button>
+        </footer-btns>
+    </div>
+</template>
+
+<script lang="ts" setup name="webInformation">
+import { getWebsite, setWebsite } from '@/api/setting/website'
+import useAppStore from '@/stores/modules/app'
+import type { FormInstance } from 'element-plus'
+const formRef = ref<FormInstance>()
+
+const appStore = useAppStore()
+// 表单数据
+const formData = reactive({
+    name: '', // 网站名称
+    web_favicon: '', // 网站图标
+    web_logo: '', // 网站logo
+    login_image: '', // 登录页广告图
+})
+
+// 表单验证
+const rules = {
+    name: [
+        {
+            required: true,
+            message: '请输入网站名称',
+            trigger: ['blur']
+        }
+    ],
+    web_favicon: [
+        {
+            required: true,
+            message: '请选择网站图标',
+            trigger: ['change']
+        }
+    ],
+    web_logo: [
+        {
+            required: true,
+            message: '请选择网站logo',
+            trigger: ['change']
+        }
+    ],
+    login_image: [
+        {
+            required: true,
+            message: '请选择登录页广告图',
+            trigger: ['change']
+        }
+    ]
+}
+
+// 获取备案信息
+const getData = async () => {
+    const data = await getWebsite()
+    for (const key in formData) {
+        //@ts-ignore
+        formData[key] = data[key]
+    }
+}
+
+// 设置备案信息
+const handleSubmit = async () => {
+    // await formRef.value?.validate()
+    await setWebsite(formData)
+    appStore.getConfig()
+    getData()
+}
+
+getData()
+</script>
+
+<style lang="scss" scoped>
+
+</style>
